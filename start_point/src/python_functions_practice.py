3b5544a4429e6bee8fa9abfdbaa97bf5d8e7893c
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-07-21 13:23:24 +0100

    functions lab started

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,89 @@
+# def return_10():
+#     return 10
+
+# def add(number_1, number_2):
+#     return number_1 + number_2
+
+# def subtract(number_1, number_2):
+#     return number_1 - number_2
+
+# def multiply(number_1, number_2):
+#     return number_1 * number_2
+
+# def divide(number_1, number_2):
+#     return number_1 / number_2
+
+# def length_of_string(test_string):
+#     return len(test_string)
+
+# def join_string(str_1, str_2):
+#     return str_1 + str_2
+
+# def add_string_as_number(string1, string2):
+#     return int(string1) + int(string2)
+
+
+
+def number_to_full_month_name(number):
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November"]
+    return months [number - 1]
+
+    # code above works for 49 to 62
+
+
+    # if int(number) == 1:
+    #     return "January"
+    
+    # elif int(number) == 2:
+    #     return "February"
+    
+    # elif int(number) == 3:
+    #     return "March"
+    
+    # elif int(number) == 4:
+    #     return "April"
+    
+    # elif int(number) == 5:
+    #     return "May"
+    
+    # elif int(number) == 6:
+    #     return "June"
+
+    # elif int(number) == 7:
+    #     return "July"
+
+    # elif int(number) == 8:
+    #     return "August"
+
+    # elif int(number) == 9:
+    #     return "September"
+
+    # elif int(number) == 10:
+    #     return "October"
+
+    # elif int(number) == 11:
+    #     return "November"
+
+    # elif int(number) == 12:
+    #     return "December"
+
+# def number_to_short_month_name(number):
+#     months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct","Nov"]
+#     return months [number - 1]
+
+# code above works for lines 64 to 77
+
+
+# def volume_of_cube(length):
+#     return length**3
+
+
+def reverse_string(string):
+    return string(::-1)
+
+
+
+
+
+
+
